b67681eff2aa0b0042f243c46db20d6586f8259b
Ahmed Abbasi <contact@ahmed.dev>
2024-10-18 01:56:50 +0200

    Update `MainActivity.kt`
    - Utilize the `Navigator` composable and pass a `NavController` to it.

--- app/src/main/java/com/example/aidify/MainActivity.kt
+++ app/src/main/java/com/example/aidify/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.example.aidify.ui.theme.AidifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +22,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             AidifyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    val navController = rememberNavController()
+                    Navigator(
+                        navController,
+                        Modifier
+                            .padding(innerPadding)
+                            .padding(20.dp)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AidifyTheme {
-        Greeting("Android")
     }
 }

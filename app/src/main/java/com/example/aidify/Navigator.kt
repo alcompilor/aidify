268a2199646cd61909cd8bb9f4e296d558f8dace
Ahmed Abbasi <contact@ahmed.dev>
2024-10-18 01:53:23 +0200

    Implement `Navigator` for screen navigation
    - Added `Navigator` composable with `NavHost` for screen navigation.
    - Configured animated transitions for entering and exiting screens.

---
+++ app/src/main/java/com/example/aidify/Navigator.kt
@@ -0,0 +1,89 @@
+package com.example.aidify
+
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.aidify.models.NavigatorAnimations
+import com.example.aidify.models.Route
+
+@Composable
+fun Navigator(navController: NavHostController, modifier: Modifier = Modifier) {
+    val animations = getAnimations()
+
+    NavHost(
+        navController = navController,
+        startDestination = Route.Welcome.name,
+        modifier = Modifier.then(modifier)
+    ) {
+        composable(
+            route = Route.Welcome.name,
+            enterTransition = animations.enterTransition,
+            exitTransition = animations.exitTransition,
+            popEnterTransition = animations.popEnterTransition,
+            popExitTransition = animations.popExitTransition
+        ) {
+        }
+
+        composable(
+            route = Route.OpenQuestions.name,
+            enterTransition = animations.enterTransition,
+            exitTransition = animations.exitTransition,
+            popEnterTransition = animations.popEnterTransition,
+            popExitTransition = animations.popExitTransition
+        ) {
+        }
+
+        composable(
+            route = Route.UNCOPE.name,
+            enterTransition = animations.enterTransition,
+            exitTransition = animations.exitTransition,
+            popEnterTransition = animations.popEnterTransition,
+            popExitTransition = animations.popExitTransition
+        ) {
+
+        }
+
+        composable(
+            route = Route.Summary.name,
+            enterTransition = animations.enterTransition,
+            exitTransition = animations.exitTransition,
+            popEnterTransition = animations.popEnterTransition,
+            popExitTransition = animations.popExitTransition
+        ) {
+
+        }
+
+        composable(
+            route = Route.EducationLibrary.name,
+            enterTransition = animations.enterTransition,
+            exitTransition = animations.exitTransition,
+            popEnterTransition = animations.popEnterTransition,
+            popExitTransition = animations.popExitTransition
+        ) {
+
+        }
+    }
+}
+
+fun getAnimations(): NavigatorAnimations {
+    return NavigatorAnimations(
+        enterTransition = {
+            slideInHorizontally(initialOffsetX = { fullWidth -> fullWidth }) + fadeIn()
+        },
+        exitTransition = {
+            slideOutHorizontally(targetOffsetX = { fullWidth -> -fullWidth }) + fadeOut()
+        },
+        popEnterTransition = {
+            slideInHorizontally(initialOffsetX = { fullWidth -> -fullWidth }) + fadeIn()
+        },
+        popExitTransition = {
+            slideOutHorizontally(targetOffsetX = { fullWidth -> fullWidth }) + fadeOut()
+        }
+    )
+}

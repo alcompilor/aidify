9ace0f545736a58725a416e4fad51025e1213cb3
DESKTOP-ME10EHD\Joseph <josephhammamii@gmail.com>
2024-10-19 20:54:49 +0200

    implemented 'get started' button to welcome screen

--- app/src/main/java/com/example/aidify/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/aidify/screens/WelcomeScreen.kt
@@ -20,7 +20,7 @@ fun WelcomeScreen(onNavigate: () -> Unit) {
     ) {
         // App title
         Text(
-            text = stringResource(id = R.string.app_name), 
+            text = stringResource(id = R.string.app_name),
             style = MaterialTheme.typography.headlineLarge,
             color = MaterialTheme.colorScheme.primary
         )

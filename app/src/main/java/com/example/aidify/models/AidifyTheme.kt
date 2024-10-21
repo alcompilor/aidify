a2c9165e33e93864c32d4bafe397a08c80d1583b
alcompilor <ahmed-abbasi@outlook.com>
2024-10-21 23:53:09 +0200

    Add `AidifyTheme` datatype to define custom theme

---
+++ app/src/main/java/com/example/aidify/models/AidifyTheme.kt
@@ -0,0 +1,35 @@
+package com.example.aidify.models
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+data class AidifyTheme(
+    val colors: CustomColorScheme,
+    val typography: CustomTypography
+)
+
+data class CustomColorScheme(
+    val primary100: Color,
+    val primary200: Color,
+    val primary300: Color,
+    val primary400: Color,
+    val primary500: Color,
+    val primaryText: Color,
+    val secondaryText: Color,
+    val background: Color,
+    val accent1: Color,
+    val accent2: Color,
+    val accent3: Color,
+)
+
+data class CustomTypography(
+    val headline: TextStyle,
+    val subheadline: TextStyle,
+    val section: TextStyle,
+    val highlight: TextStyle,
+    val input: TextStyle,
+    val paragraph: TextStyle,
+    val clickable: TextStyle,
+    val notice: TextStyle,
+    val tag: TextStyle
+)

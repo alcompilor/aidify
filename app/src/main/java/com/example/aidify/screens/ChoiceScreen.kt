2277fade0b84e28bf00df929cf628a38cf08c921
DESKTOP-ME10EHD\Joseph <josephhammamii@gmail.com>
2024-10-29 20:48:18 +0100

    implementation of the choice screen

---
+++ app/src/main/java/com/example/aidify/screens/ChoiceScreen.kt
@@ -0,0 +1,42 @@
+package com.example.aidify.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.aidify.models.Route
+import com.example.aidify.ui.theme.aidifyTheme
+
+@Composable
+fun ChoiceScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Choose your path",
+            style = aidifyTheme.typography.headline,
+            color = aidifyTheme.colors.primaryText,
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { navController.navigate(Route.UNCOPE.name) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        ) {
+            Text(text = "I'm struggling with addiction")
+        }
+
+    }
+}

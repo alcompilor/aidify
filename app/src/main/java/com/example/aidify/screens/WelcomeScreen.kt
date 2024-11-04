9fc0b9e0908117e54df558bdd73e13cf09e81fed
alcompilor <ahmed-abbasi@outlook.com>
2024-11-04 19:13:58 +0100

    Add logo
    - Edit `WelcomeScreen` to use Lotusmodellen logo
    - Edit app icon to use Lotusmodellen logo

--- app/src/main/java/com/example/aidify/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/aidify/screens/WelcomeScreen.kt
@@ -1,12 +1,17 @@
 package com.example.aidify.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.LocalLibrary
 import androidx.compose.material.icons.outlined.Start
@@ -15,14 +20,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.aidify.R
 import com.example.aidify.models.Route
 import com.example.aidify.ui.components.LanguageSpinner
+import com.example.aidify.ui.components.Logo
 import com.example.aidify.ui.components.NextScreenBtn
 import com.example.aidify.ui.components.ScreenTitle
 import com.example.aidify.ui.theme.aidifyTheme
@@ -58,11 +67,14 @@ fun WelcomeScreen(viewmodel: WelcomeViewModel, navController: NavController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            ScreenTitle(
-                title = stringResource(R.string.app_name),
-                icon = Icons.Rounded.Category
+            Logo(
+                text = stringResource(R.string.app_name),
+                image = R.drawable.logo_lotusmodellen,
+                width = 122.2.dp,
+                height = 65.dp,
+                spacer = 2.5.dp
             )
-            Spacer(modifier = Modifier.padding(3.dp))
+            Spacer(modifier = Modifier.padding(6.dp))
             Text(
                 text = stringResource(R.string.welcome_headline),
                 style = aidifyTheme.typography.subheadline,
